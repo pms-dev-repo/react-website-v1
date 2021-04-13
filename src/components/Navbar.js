a90d88ced391a6eee9a375c2b7a8e4af6ac7d4c3
Hugo Uzabeaga <huzabeaga@gmail.com>
2021-04-12 23:27:49 -0400

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -60,6 +60,18 @@ function Navbar() {
               </Link>
             </li>
 
+            <li className='nav-item'>
+              <Link
+                to='/products'
+                className='nav-links'
+                onClick={closeMobileMenu}
+              >
+                Podcast / Blog Viajero
+              </Link>
+            </li>
+
+            
+
             <li>
               <Link
                 to='/sign-up'
